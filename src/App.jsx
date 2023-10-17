3a9ade33f4f0d3adcc5872b751dcf606353d7563
SaranSenthil <nrms162@gmail.com>
2023-10-17 14:28:01 +0530

    dynamic content added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
 
 function App(){
-  console.log("Hello ")
+  // console.log("Hello ")
+  const now = new Date();
+  //Simple addition
+
+  const a=10, b=20;
   return (
     <div>
-    <p> Hello world!!</p>
+    <p> Hello world!! The time is {now.toString()}</p>
+    <p>
+      {a} plus {b} is {a+b}
+    </p>
     </div>
   )
 }
