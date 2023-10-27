5ca729eacf74e596634f4b43409136c29450881c
SaranSenthil <nrms162@gmail.com>
2023-10-27 16:30:39 +0530

    functional comp vs Class comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
+//functional component - created a JS function and it should return jsx (html with js embeded)
+// function App() {
+//   return (
+//     <div>App</div>
+//   )
+//   }
+// export default App
 
-function App() {
-  return (
-    <div>App</div>
-  )
+//Class Component - extends is a inherit keyword
+
+class App extends Component{
+ //method 
+  render(){
+    return (
+          <div>Hello World</div>
+         )
+  }
 }
 
-export default App
+export default App;
