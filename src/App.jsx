d3b33b38258ec79718539106db90054e15e94402
SaranSenthil <nrms162@gmail.com>
2023-11-10 11:55:16 +0530

    Login and register form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,16 @@
-import React, { Component } from 'react'
-
-//State : state refers to the data. ie) used to manage and represent the internal state of the component
-
-class App extends Component {
-  constructor(props)
-  {    //to call the parent class constructor
-    super(props);
-
-    //define states:
-    this.state={
-votes: 0,
-    }
-  }
-  increment=()=>{
-    //update the state
-     this.setState({
-      votes:this.state.votes+1,
-    })
-  }
-  decrement=()=>{
-    //update the state
-     this.setState({
-      votes:this.state.votes-1,
-    })
-  }
-  reset=()=>{
-    //update the state
-     this.setState({
-      votes:0,
-    })
-  }
-  render() {
-    return (
-      <div>
-        <p>
-          Votes: {this. state.votes}
-        </p>
-      
-       <div className="d-flex justify-content-evenly" >
-        <button onClick={this.increment}>Click to Increment</button>
-        <button onClick={this.decrement}>Click to Decrement</button>
-        <button onClick={this.reset}>Reset</button>
-        </div>
-        
-      </div>
-    )
-  }
+import React from 'react'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+import {Route,Routes} from 'react-router-dom'
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<SignUp/>} />
+      <Route path='/Login' element={<Login/>} />
+    </Routes>
+    </>
+  )
 }
 
 export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+import {Container,Form,Button, FormGroup, FormLabel} from 'react-bootstrap';
+import "../Styles/Login.css";
+import {Link,useNavigate} from 'react-router-dom' ;
+
+const Login = () => {
+  const [formData,setForm]= useState({
+       email:" ",
+    password:""
+  })
+  const handleChange=(e)=>{
+    const{name,value}= e.target;
+    setForm({...formData,[name]:value});
+      };
+    
+      const handlesubmit=(e)=>{
+        e.preventDefault();
+    console.log(formData);
+      }
+  return (    
+   <Container>
+    <h1> Login </h1>
+      <Form onSubmit={handlesubmit}>
+    <Form.Group>
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" name="email"  value={formData.email} onChange={handleChange} required />
+    </Form.Group>
+    <Form.Group>
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" name="password"  value={formData.password} onChange={handleChange} required />
+    </Form.Group>
+    <Button variant="primary" type="submit">Login</Button>
+    </Form>
+   </Container>
+  );
+};
+
+export default Login;
